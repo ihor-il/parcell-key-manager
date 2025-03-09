362ce411b1f5fd349df6f1aee34a45b488b36816
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2025-03-09 19:19:37 +0200

    added capacitor project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ihorlupyna.parcell',
+  appName: 'parcell-key-manager',
+  webDir: 'dist/parcell-key-manager/browser'
+};
+
+export default config;
