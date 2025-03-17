b1150e780ac7a6216fe3b19651c1863c0bb22377
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2025-03-17 22:43:32 +0200

    updated bottom bar

--- src/app/components/bottom-bar/bottom-bar.component.ts
+++ src/app/components/bottom-bar/bottom-bar.component.ts
@@ -3,6 +3,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ChangeDetectionStrategy, Component, ElementRef } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { bottomBarConfig } from './bottom-bar.config';
+import { KeyValuePipe, NgClass } from '@angular/common';
 
 @Component({
     imports: [
@@ -11,10 +13,14 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
         MatButtonModule,
         RouterLink,
         RouterLinkActive,
+        KeyValuePipe,
+        NgClass
     ],
     selector: 'app-bottom-bar',
     templateUrl: './bottom-bar.component.html',
     styleUrl: './bottom-bar.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BottomBarComponent {}
+export class BottomBarComponent {
+    readonly config = bottomBarConfig;
+}
---
+++ src/app/components/bottom-bar/bottom-bar.config.ts
@@ -0,0 +1,5 @@
+export const bottomBarConfig = {
+    '2fa': 'lock',
+    passwords: 'vpn_key',
+    settings: 'settings',
+};
