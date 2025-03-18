2c0734254e5c15b1ba8de891dba600deaac50583
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2025-03-19 01:06:06 +0200

    updated bottom bar styling

--- src/app/components/bottom-bar/bottom-bar.component.ts
+++ src/app/components/bottom-bar/bottom-bar.component.ts
@@ -1,10 +1,10 @@
+import { KeyValuePipe } from '@angular/common';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ChangeDetectionStrategy, Component, ElementRef } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { bottomBarConfig } from './bottom-bar.config';
-import { KeyValuePipe, NgClass } from '@angular/common';
 
 @Component({
     imports: [
@@ -14,12 +14,10 @@ import { KeyValuePipe, NgClass } from '@angular/common';
         RouterLink,
         RouterLinkActive,
         KeyValuePipe,
-        NgClass
     ],
     selector: 'app-bottom-bar',
     templateUrl: './bottom-bar.component.html',
     styleUrl: './bottom-bar.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BottomBarComponent {
     readonly config = bottomBarConfig;
