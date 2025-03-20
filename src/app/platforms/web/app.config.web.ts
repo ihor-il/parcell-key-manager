464047fce56cc1ba5eb55860f101ca8dc7ba1f57
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2025-03-20 21:44:01 +0200

    deleted platforms

--- src/app/platforms/web/app.config.web.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { baseConfig } from 'app/app.config.base';
-import { webProviders } from 'app/platforms/web/providers';
-
-export const appConfig: ApplicationConfig = {
-    providers: baseConfig.providers.concat(webProviders),
-};
--- src/app/platforms/web/providers.ts
+++
@@ -1,7 +0,0 @@
-import { Provider } from '@angular/core';
-import { PASSWORD_SERVICE } from 'app/services/password.service';
-import { PasswordService } from './services/web-password.service';
-
-export const webProviders: Provider[] = [
-    { provide: PASSWORD_SERVICE, useClass: PasswordService },
-];
